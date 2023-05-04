e510d452febde3c3d8cdcbdb51dbaf8a321c90bf
imronarrosid <imronbaru007@gmail.com>
2023-05-04 15:08:54 +0000

    Fixs not paid controller

--- lib/controller/not_paid_controller.dart
+++ lib/controller/not_paid_controller.dart
@@ -13,14 +13,17 @@ Future<void> resetNotPaid() async {
     var item = persons[index];
     var notPaid=int.parse(item.paid)-int.parse(item.notPaid)> Payment.getHaveToPaid()?0:paymentAmount;
     var reseted = int.parse(item.notPaid) + notPaid;
+    var haveToPaid = Payment.getHaveToPaid() + Payment.getAmount();
     var personObj = Person(
       id: item.id,
         name: item.name,
         paid: item.paid,
         notPaid: reseted.toString(),
         createdAt: DateTime.now().toString());
-        Payment.setHaveToPaid(reseted);
+
+        Payment.setHaveToPaid(haveToPaid);
     await DatabaseHelper.instance.update(personObj);
+    print('${item.name} ${item.notPaid}');
   }
   
   }
