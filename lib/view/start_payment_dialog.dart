b5e1720ab8b18789625f80779410de3a01b59be8
imronarrosid <imronbaru007@gmail.com>
2023-05-13 13:59:23 +0000

    remove unused import

--- lib/view/start_payment_dialog.dart
+++ lib/view/start_payment_dialog.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pembayaran_kas/controller/start_button_controller.dart';
 import 'package:pembayaran_kas/service/notification.dart';
-import 'package:pembayaran_kas/view/home.dart';
 import 'package:pembayaran_kas/view/root_page.dart';
 
 Future<void> startPaymentDialog(BuildContext context) async {
