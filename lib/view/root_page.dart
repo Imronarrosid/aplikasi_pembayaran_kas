c9bbe0f69570c8a0eb1ff258fa44dc728bbba1de
imronarrosid <imronbaru007@gmail.com>
2023-05-13 14:00:05 +0000

    remove unused import

--- lib/view/root_page.dart
+++ lib/view/root_page.dart
@@ -1,7 +1,5 @@
 import 'package:entry/entry.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:iconly/iconly.dart';
 import 'package:pembayaran_kas/model/payment.dart';
 import 'package:pembayaran_kas/view/create_cash_out.dart';
